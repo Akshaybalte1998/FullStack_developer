7560cdae49192858420d5b7854c4ae7b21b077ef
Akshaybalte1998 <balteakshay1998@gmail.com>
2024-08-05 22:29:46 +0530

    adding new code

---
+++ PrepInsta100Codes/src/com/simpleCodes/PostiveNeagativeNumber.java
@@ -0,0 +1,25 @@
+package com.simpleCodes;
+
+import java.util.Scanner;
+
+public class PostiveNeagativeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number to check number is postive or neagtive ot zero ");
+		int number=scan.nextInt();
+		
+		//brute force
+		
+		if(number>0)
+			System.out.println("Number is positive number : "+number);
+		else if(number<0)
+			System.out.println("Number is negative number"+number);
+		else
+			System.out.println("zero");
+
+		
+	}
+
+}
