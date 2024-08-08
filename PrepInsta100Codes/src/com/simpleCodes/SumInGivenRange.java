d66d4f1eb0267f66e25a3054bfe42e6c19b6a6c1
Akshaybalte1998 <balteakshay1998@gmail.com>
2024-08-08 23:26:47 +0530

    added

---
+++ PrepInsta100Codes/src/com/simpleCodes/SumInGivenRange.java
@@ -0,0 +1,22 @@
+package com.simpleCodes;
+
+import java.util.Scanner;
+
+public class SumInGivenRange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number where range start:  ");
+		int startRange=scan.nextInt();
+		System.out.println("enter the number where range end:  ");
+		int endRange=scan.nextInt();
+		int sum=0;
+		for(int i=startRange;i<=endRange;i++)
+		{
+			sum+=i;
+		}
+		System.out.println(" sum of the given range is :"+sum);
+	}
+
+}
